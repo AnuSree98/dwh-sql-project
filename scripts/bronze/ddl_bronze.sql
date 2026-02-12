8f27f8eab407c3ca620f36742e118bfb89e57ff2
AnuSree98 <50586397+AnuSree98@users.noreply.github.com>
2026-02-12 23:06:45 +1100

    Create ddl_bronze.sql

---
+++ scripts/bronze/ddl_bronze.sql
@@ -0,0 +1,52 @@
+/* This script is used to create the tables in the bronze schema. Run this script to re-define the DDL structure of bronze tables */
+
+CREATE TABLE bronze.crm_cust_info(
+	cst_id int,
+	cst_key nvarchar(50),
+	cst_firstname nvarchar(50),
+	cst_lastname nvarchar(50),
+	cst_marital_status nvarchar(50),
+	cst_gndr nvarchar(50),
+	cst_create_date date
+);
+
+CREATE TABLE bronze.crm_prd_info(
+	prd_id int,
+	prd_key nvarchar(50),
+	prd_nm nvarchar(50),
+	prd_cost int,
+	prd_line nvarchar(50),
+	prd_start_dt date,
+	prd_end_dt date
+);
+
+CREATE TABLE bronze.crm_sales_details(
+	sls_ord_num nvarchar(50),
+	sls_prd_key nvarchar(50),
+	sls_cust_id int,
+	sls_order_dt int,
+	sls_ship_dt int,
+	sls_due_dt int,
+	sls_sales int,
+	sls_quantity int,
+	sls_price int
+);
+
+CREATE TABLE bronze.erp_cust(
+	CID nvarchar(50),
+	BDATE date,
+	GEN nvarchar(50)
+);
+drop table bronze.erp_loc;
+CREATE TABLE bronze.erp_loc(
+	CID nvarchar(50),
+	CNTRY nvarchar(50)
+);
+
+CREATE TABLE bronze.erp_px_cat(
+	ID nvarchar(50),
+	CAT nvarchar(50),
+	SUBCAT nvarchar(50),
+	MAINTENANCE nvarchar(50)
+);
+
